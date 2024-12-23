5cc0ad02aa080cb66207fab4fea7ebf76fd2048a
fredericoaugusto <fred3112.augusto@gmail.com>
2024-12-23 16:48:20 -0300

    Configurações do formulario Configurações do BD

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AuthController extends Controller
 {
@@ -11,8 +12,35 @@ class AuthController extends Controller
         return view('login');
     }
 
+    public function loginSubmit(Request $request)
+    {
+        $request->validate([
+            'text_username' => 'required|email',
+            'text_password' => 'required|min:6|max:16'
+        ], [
+            'text_username.required' => 'O username é obrigatório',
+            'text_username.email' => 'O username deve ser um email válido',
+            'text_password.required' => 'A senha é obrigatória',
+            'text_password.min' => 'A senha deve ter no mínimo :min caracteres',
+            'text_password.max' => 'A senha deve ter no máximo :max caracteres'
+        ]);
+
+
+
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+        try {
+            DB::connection()->getPdo();
+            echo 'Conexão funcionando';
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+        }
+
+
+    }
     public function logout()
     {
-        return view('logout');
+        echo 'Logout';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-// Auth routes
 
+// Auth routes
 Route::get('/login', [AuthController::class, 'login']);
+Route::post('/loginSubmit', [AuthController::class, 'loginSubmit'])->name('loginSubmit');
 Route::get('/logout', [AuthController::class, 'logout']);
