02d68cc7aa76bd38171c22cfeafc3962c0e6c138
fredericoaugusto <fred3112.augusto@gmail.com>
2024-12-26 09:16:51 -0300

    Criação e implementação de middleware para verificar se usuário está logado ou não logado e direcionar para a página correta.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
+    public function index()
+    {
+        echo 'Página inicial';
+    }
 
+    public function newNote()
+    {
+        echo 'Nova nota';
+    }
 }
