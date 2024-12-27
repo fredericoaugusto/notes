f4246427db9eb56ddec6cb87ec39eaf2d7936a8c
fredericoaugusto <fred3112.augusto@gmail.com>
2024-12-27 16:37:38 -0300

    Criação de form de cadastrar nova nota; Criação das rotas para submissão de formulário de nova nota e também para a página do formulário; Configurações no controller para newNote.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Services\Operations;
 use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
@@ -20,31 +21,25 @@ class MainController extends Controller
 
     public function newNote()
     {
-        echo 'Nova nota';
+        return view('new_note');
+    }
+
+    public function newNoteSubmit(Request $request)
+    {
+        echo 'Criando nova nota';
     }
 
     public function editNote($id)
     {
-        $id = $this->decryptId($id);
+        $id = Operations::decryptId($id);
 
         echo 'Editar nota ' . $id;
     }
 
     public function deleteNote($id)
     {
-        $id = $this->decryptId($id);
+        $id = Operations::decryptId($id);
 
         echo 'Deletar nota ' . $id;
-    }
-
-    private function decryptId($id)
-    {
-        try {
-            $id = Crypt::decrypt($id);
-        } catch (DecryptException $e) {
-            return redirect()->route('home');
-        }
-
-        return $id;
     }
 }
